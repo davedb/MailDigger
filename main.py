f8da239d3c021b7977b0b494326b5801765d782f
Davide <davide@davide-K61IC.lan>
2016-02-15 00:31:57 +0100

    recuperate le email, sono riuscito a leggerne il contenuto, manca l'estrazione dei dati

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import oauth2client
 from oauth2client import client
 from oauth2client import tools
 
+import base64
+import re
+from bs4 import BeautifulSoup
+
+
 try:
     import argparse
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
@@ -17,18 +22,18 @@ except ImportError:
 # at ~/.credentials/gmail-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Gmail API Python Quickstart'
+APPLICATION_NAME = 'MailDigger'
 
 
 def get_credentials():
-    """Gets valid user credentials from storage.
+    '''Gets valid user credentials from storage.
 
     If nothing has been stored, or if the stored credentials are invalid,
     the OAuth2 flow is completed to obtain the new credentials.
 
     Returns:
         Credentials, the obtained credential.
-    """
+    '''
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
@@ -58,15 +63,33 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
 
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
+    #results = service.users().labels().list(userId='me').execute()
+    results = service.users().messages().list(userId='me', q='bikemi').execute()
+    messages = results.get('messages', [])
+    #print(messages)
 
-    if not labels:
-        print('No labels found.')
+    if not messages:
+        print('No messages found.')
     else:
-      print('Labels:')
-      for label in labels:
-        print(label['name'])
+        print('messages:')
+        for message in messages:
+            messageId = message['id']
+            print(messageId)#, service.users().messages().get(userId='me', id=messageId).execute())
+            if messageId == '152c0033f0e6ac3a':
+                cMessage = service.users().messages().get(userId='me', id=messageId, format='full').execute()
+                #print(cMessage['payload']['body']['data'])
+                cMessageDataDecoded = base64.urlsafe_b64decode(cMessage['payload']['body']['data'])
+                soup = BeautifulSoup(cMessageDataDecoded, 'html.parser')
+                bodyContentArray = soup.body.contents
+                for content in bodyContentArray:
+                    print(type(content.string))
+                #    if re.search('ORA PRELIEVO', content):
+                #        print('oraPrelievo >>> ',content)
+               # oraPrelievo = re.search('>ORA PRELIEVO: (.*?)<br', soup.body.string)
+               # print('oraPrelievo >>> ',oraPrelievo.group(1))
+              #  print('cMessageDataDecoded >>> ',soup.body.contents)
+
+    
 
 
 if __name__ == '__main__':
